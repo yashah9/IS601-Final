a95f32200dd88171de2b17cc4f345a63bb1a216b
yashah9 <yashshah0910@gmail.com>
2025-04-28 21:29:44 -0400

    fixes for minio

--- settings/config.py
+++ settings/config.py
@@ -41,7 +41,13 @@ class Settings(BaseSettings):
     smtp_username: str = Field(default='your-mailtrap-username', description="Username for SMTP server")
     smtp_password: str = Field(default='your-mailtrap-password', description="Password for SMTP server")
 
-
+    # minoi config
+    MINIO_ENDPOINT: str = Field(default= "minio:9000", description="Minio endpoint")
+    MINIO_ACCESS_KEY: str = Field(default= "your_minio_access_key", description="Minio access key")
+    MINIO_SECRET_KEY: str = Field(default= "your_minio_secret_key", description="Minio secret key")
+    MINIO_USE_SSL: bool = Field(default= False, description="Minio use ssl")
+    MINIO_BUCKET_NAME: str = Field(default= "picture", description="Minio bucket name")
+    
     class Config:
         # If your .env file is not in the root directory, adjust the path accordingly.
         env_file = ".env"
