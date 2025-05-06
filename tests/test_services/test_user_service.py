873870dc9f00684127c3fb03da3db693c84567af
yashah9 <yashshah0910@gmail.com>
2025-05-05 23:50:10 -0400

    tests for missing , invalid email

--- tests/test_services/test_user_service.py
+++ tests/test_services/test_user_service.py
@@ -4,14 +4,14 @@ from sqlalchemy import select
 from app.dependencies import get_settings
 from app.models.user_model import User, UserRole
 from app.services.user_service import UserService
-from app.utils.nickname_gen import generate_nickname
+from app.utils.nickname_gen import generate_nickname_with_id
 
 pytestmark = pytest.mark.asyncio
 
 # Test creating a user with valid data
 async def test_create_user_with_valid_data(db_session, email_service):
     user_data = {
-        "nickname": generate_nickname(),
+        "nickname": generate_nickname_with_id(),
         "email": "valid_user@example.com",
         "password": "ValidPassword123!",
         "role": UserRole.ADMIN.name
@@ -95,7 +95,7 @@ async def test_list_users_with_pagination(db_session, users_with_same_role_50_us
 # Test registering a user with valid data
 async def test_register_user_with_valid_data(db_session, email_service):
     user_data = {
-        "nickname": generate_nickname(),
+        "nickname": generate_nickname_with_id(),
         "email": "register_valid_user@example.com",
         "password": "RegisterValid123!",
         "role": UserRole.ADMIN
@@ -161,3 +161,31 @@ async def test_unlock_user_account(db_session, locked_user):
     assert unlocked, "The account should be unlocked"
     refreshed_user = await UserService.get_by_id(db_session, locked_user.id)
     assert not refreshed_user.is_locked, "The user should no longer be locked"
+
+# Test account locking with invalid login attempts
+async def test_account_lock_with_invalid_logins(db_session, verified_user):
+    max_login_attempts = get_settings().max_login_attempts
+    for _ in range(max_login_attempts):
+        await UserService.login_user(db_session, verified_user.email, "wrongpassword")
+    
+    is_locked = await UserService.is_account_locked(db_session, verified_user.email)
+    assert is_locked is True, "User account should be locked after max failed login attempts."
+
+# Test user registration with missing email
+async def test_register_user_missing_email(db_session, email_service):
+    user_data = {
+        "password": "ValidPassword123!",
+        "role": UserRole.ADMIN.name
+    }
+    user = await UserService.register_user(db_session, user_data, email_service)
+    assert user is None  # Registration should fail due to missing email
+
+# Test user registration with invalid email format
+async def test_register_user_invalid_email_format(db_session, email_service):
+    user_data = {
+        "email": "invalidemailformat",
+        "password": "ValidPassword123!",
+        "role": UserRole.ADMIN.name
+    }
+    user = await UserService.register_user(db_session, user_data, email_service)
+    assert user is None  # Registration should fail due to invalid email format
